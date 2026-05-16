11bda8575c016048de30a98b77f9759549a06c8c
Soham Lokhande <146424231+sohamlokhande9@users.noreply.github.com>
2026-05-16 11:02:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// This file is intentionally simple
+// for beginner-level understanding.
+
+// You can add future JavaScript features here.
+
+console.log("Portfolio Website Loaded Successfully");
